239a00f53e97eac1b517adfd48f14ec5feea6fba
Kasia Dam <katarzynadam@gmail.com>
2017-01-25 21:37:15 +0100

    Add files via upload

---
+++ exercise1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+  fmt.Println("Kasia_" + "Dam")
+  fmt.Println("my_" + "first_" + "program_" + "in_" + "GO_" + "_language")
+  
+  fmt.Println("7 * 3 =", 7 * 3)
+  fmt.Println("7 + 3 =", 7 + 3)
+  fmt.Println("4 : 1.5", 4.0 / 1.5)
+  
+  fmt.Println(false && false)
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(false || false)
+  
+  
+}
